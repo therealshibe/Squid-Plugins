83db58c7978f69fef377ea14b4e93a60a4e6054a
Francis Taylor <fishyfing@gmail.com>
2016-05-29 20:19:36 +0100

    Moved check to top

--- admin/admin.py
+++ admin/admin.py
@@ -49,19 +49,19 @@ class Admin:
 
         Can use `message`, `channel`, `server`, and `discord`
         """
-        try:
-            evald = eval(text, {}, {'message': ctx.message,
-                                    'channel': ctx.message.channel,
-                                    'server': ctx.message.server,
-                                    'discord': discord})
-        except:
-            evald = text
-        if len(str(evald)) > 2000:
-            evald = str(evald)[:1990] + " you fuck."
         user = ctx.message.author
         if user.bot == True
             return
         else:
+            try:
+                evald = eval(text, {}, {'message': ctx.message,
+                                        'channel': ctx.message.channel,
+                                        'server': ctx.message.server,
+                                        'discord': discord})
+            except:
+                evald = text
+            if len(str(evald)) > 2000:
+                evald = str(evald)[:1990] + " you fuck."
             await self.bot.say(evald)
 
     @commands.command(pass_context=True)
