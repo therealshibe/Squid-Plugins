cbfb7715ad656beec2231f875c52514f8ee843ff
Will Tekulve <tekulve.will@gmail.com>
2016-05-29 15:07:09 -0400

    Announce

--- admin/admin.py
+++ admin/admin.py
@@ -2,7 +2,11 @@ import discord
 from discord.ext import commands
 from cogs.utils import checks
 from __main__ import settings
-from copy import deepcopy
+import asyncio
+import logging
+
+
+log = logging.getLogger("red.admin")
 
 
 class Admin:
@@ -10,6 +14,7 @@ class Admin:
 
     def __init__(self, bot):
         self.bot = bot
+        self._announce_msg = None
 
     @commands.command(no_pm=True, pass_context=True)
     @checks.admin_or_permissions(manage_roles=True)
@@ -37,6 +42,16 @@ class Admin:
 
         await self.bot.add_roles(user, role)
         await self.bot.say('Added role {} to {}'.format(role.name, user.name))
+
+    @commands.command(pass_context=True)
+    @checks.is_owner()
+    async def announce(self, ctx, *, msg):
+        """Announces a message to all servers that a bot is in."""
+        if self._announce_msg is not None:
+            await self.bot.say("Already announcing, wait until complete to"
+                               " issue a new announcement.")
+        else:
+            self._announce_msg = msg
 
     @commands.command(no_pm=True, pass_context=True)
     @checks.admin_or_permissions(manage_roles=True)
@@ -70,6 +85,32 @@ class Admin:
         new_msg.content = self.bot.command_prefix[0] + command
         await self.bot.process_commands(new_msg)
 
+    async def announcer(self, msg):
+        server_ids = map(lambda s: s.id, self.bot.servers)
+        for server_id in server_ids:
+            server = self.bot.get_server(server_id)
+            if server is None:
+                continue
+            chan = server.default_channel
+            log.debug("Looking to announce to {} on {}".format(chan.name,
+                                                               server.name))
+            me = server.me
+            if chan.permissions_for(me).send_messages:
+                log.debug("I can send messages to {} on {}, sending".format(
+                    server.name, chan.name))
+                await self.bot.send_message(chan, msg)
+            await asyncio.sleep(1)
+
+    async def announce_manager(self):
+        while self == self.bot.get_cog('Admin'):
+            if self._announce_msg is not None:
+                log.debug("Found new announce message, announcing")
+                await self.announcer(self._announce_msg)
+                self._announce_msg = None
+            await asyncio.sleep(1)
+
 
 def setup(bot):
-    bot.add_cog(Admin(bot))
+    n = Admin(bot)
+    bot.add_cog(n)
+    bot.loop.create_task(n.announce_manager())
