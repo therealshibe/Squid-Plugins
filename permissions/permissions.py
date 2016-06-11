fc39e1d0e7753f06e8c6ca78bb59643439f9cbb8
Will Tekulve <tekulve.will@gmail.com>
2016-06-11 16:21:29 -0400

    Lot's of helpers, possibly a working check generator

--- permissions/permissions.py
+++ permissions/permissions.py
@@ -3,16 +3,25 @@ from discord.ext import commands
 from cogs.utils.dataIO import dataIO
 from cogs.utils import checks
 import os
+import logging
+
+log = logging.getLogger("red.permissions")
 
 
-class BadCommand(Exception):
+class PermissionsError(Exception):
+    """
+    Base exception for all others in this module
+    """
+
+
+class BadCommand(PermissionsError):
     """
     Thrown when we can't decipher a command from string into a command object.
     """
     pass
 
 
-class RoleNotFound(Exception):
+class RoleNotFound(PermissionsError):
     """
     Thrown when we can't get a valid role from a list and given name
     """
@@ -37,16 +46,64 @@ class Permissions:
 
     def __init__(self, bot):
         self.bot = bot
-        self.perms = self._load_perms()
 
-    def _load_perms(self):
-        try:
-            ret = dataIO.load_json("data/permissions/perms.json")
-        except:
-            ret = {}
-            os.mkdir("data/permissions")
-            dataIO.save_json("data/permissions/perms.json", ret)
-        return ret
+        # All the saved permission levels with role ID's
+        self.perms_we_want = self._load_perms()
+
+        # All the checks we've added into Command objects
+        self.perm_check_map = {}
+
+    def _check(self, serverid, roleid):
+        def has_higher_role(member, role):
+            server = member.server
+            roles = self._get_ordered_role_list(server=server)
+            try:
+                role_index = roles.index(role)
+            except ValueError:
+                # Role isn't in the ordered role list
+                return False
+
+            higher_roles = roles[role_index + 1:]
+
+            if any([r in higher_roles for r in member.roles]):
+                return True
+            return False
+
+        def pred(ctx):
+            server = self._get_server_from_id(serverid)
+            role = self._get_role_from_id(roleid)
+            msg = "Check on `{}`".format(ctx.command.qualified_name)
+            # Ignore in pm
+            if ctx.message.channel.is_private:
+                return True
+
+            # Our old server doesn't exist
+            if server not in ctx.bot.servers:
+                log.debug(msg + " failed: server not in bot serverlist")
+                return True
+
+            # This is not the server you are looking for
+            if ctx.message.server != server:
+                log.debug(msg + " failed: check server does not equal"
+                                " current server")
+                return True
+
+            # Role has been deleted
+            if role not in ctx.message.server.roles:
+                log.debug(msg + " failed: role has been deleted")
+                return True
+
+            if role in ctx.message.author.roles or \
+                    has_higher_role(ctx.message.author, role):
+                log.debug(msg + " succeeded: aid {} has permissions".format(
+                    ctx.message.author.id))
+                return True
+            else:
+                log.debug(
+                    msg + " succeeded: aid {} does not have permission".format(
+                        ctx.message.author.id))
+                return False
+        return pred
 
     def _error_raise(exc):
         def deco(func):
@@ -76,6 +133,20 @@ class Permissions:
             ret = ret.commands[cmd.pop(0)]
         return ret
 
+    def _get_ordered_role_list(self, server=None, roles=None):
+        if server is None and roles is None:
+            raise PermissionsError("Must supply either server or role.")
+
+        if server:
+            roles = server.roles
+
+        ordered_roles = sorted(roles, key=lambda r: r.position)
+
+        log.debug("Ordered roles for sid {}:\n\t{}".format(server.id,
+                                                           ordered_roles))
+
+        return sorted(roles, key=lambda r: r.position)
+
     def _get_role(self, roles, role_string):
         role = discord.utils.find(
             lambda r: r.name.lower() == role_string.lower(), roles)
@@ -83,14 +154,43 @@ class Permissions:
         if role is None:
             raise RoleNotFound(roles[0].server, role_string)
 
+    def _get_role_from_id(self, server, roleid):
+        try:
+            roles = server.roles
+        except AttributeError:
+            server = self._get_server_from_id(server)
+            try:
+                roles = server.roles
+            except AttributeError:
+                raise RoleNotFound(server, roleid)
+
+        role = discord.utils.get(roles, id=roleid)
+        if role is None:
+            raise RoleNotFound(server, roleid)
+        return role
+
+    def _get_server_from_id(self, serverid):
+        return discord.utils.get(self.bot.servers, id=serverid)
+
+    def _load_perms(self):
+        try:
+            ret = dataIO.load_json("data/permissions/perms.json")
+        except:
+            ret = {}
+            os.mkdir("data/permissions")
+            dataIO.save_json("data/permissions/perms.json", ret)
+        return ret
+
     @commands.group(pass_context=True)
     @checks.serverowner_or_permissions(manage_roles=True)
     async def p(self, ctx):
+        """Permissions manager"""
         if ctx.invoked_subcommand is None:
             return
 
     @p.command(pass_context=True, name="set")
     async def _set(self, ctx, command, *, role):
+        """Require a command to (at minimum) be run by the specified role"""
         server = ctx.message.server
         command_obj = self._get_command(command)
         role = self._get_role(server.roles, role)
